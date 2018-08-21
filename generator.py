2869bd095cecd6e9954029c89887ded31ebbe717
Andre Filho <andre.filho001@outlook.com>
2018-08-21 17:19:35 -0300

    fix(script): script part responsible for the bash

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
-from subprocess import call
+# import subprocess
+import os
 
 tag_type = int(input("""
 what type of commit convention are you using?
@@ -10,14 +11,14 @@ what type of commit convention are you using?
 
 tag = str(input("type the tag: "))
 msg = str(input("type the commit message: "))
-comment = str(input('type additional comments (if needed): '))
+# comment = str(input('type additional comments (if needed): '))
 
 commit_msg_pre = 'git commit -m '
 
 if tag_type == 1:
     context = str(input('type the namespace: '))
-    composed_msg = commit_msg_pre + "'" + tag + "(" + context + ")" +": " + msg + "\n" + comment + "'"
-    subprocess.call(composed_msg)
+    composed_msg = commit_msg_pre + "'" + tag + "(" + context + ")" +": " + msg + "'"
+    os.system(composed_msg)
 else:
-    composed_msg = commit_msg_pre + "'" + tag + ": " + msg + "\n" + comment + "'"
-    subprocess.call(composed_msg)
+    composed_msg = commit_msg_pre + "'" + tag + ": " + msg + "'"
+    os.system(composed_msg)
