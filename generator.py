044ccd9da1fb2cf8b458fa63451a7768ed9ac4f8
Andre Filho <andre.filho001@outlook.com>
2018-08-21 19:44:08 -0300

    add: refactorates method of string interpolation

--- generator.py
+++ generator.py
@@ -10,14 +10,9 @@ what type of commit convention are you using?
 tag = str(input("type the tag: "))
 msg = str(input("type the commit message: ")).lower()
 
-commit_msg_pre = 'git commit -m '
-
 if tag_type == 1:
     context = str(input('type the context: ')).lower()
     tag = tag.lower()
-    composed_msg = commit_msg_pre + "'" + tag + "(" + context + ")" +": " + msg + "'"
-    # os.system(composed_msg)
     os.system("git commit -m '%s(%s): %s'" % (tag, context, msg))
 else:
-    composed_msg = commit_msg_pre + "'" + tag.upper() + ": " + msg + "'"
-    os.system(composed_msg)
+    os.system("git commit -m '%s: %s'" % (tag, msg))
