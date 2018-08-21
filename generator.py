35b14a3fd58d0af3f6c8d4ab39b8c978982bb53d
Andre Filho <andre.filho001@outlook.com>
2018-08-21 17:27:34 -0300

    update(script): prevents unformatted commit messages

--- generator.py
+++ generator.py
@@ -5,19 +5,19 @@ what type of commit convention are you using?
 
 1- angular
 2- changelog
-
 """))
 
 tag = str(input("type the tag: "))
-msg = str(input("type the commit message: "))
+msg = str(input("type the commit message: ")).lower()
 # comment = str(input('type additional comments (if needed): '))
 
 commit_msg_pre = 'git commit -m '
 
 if tag_type == 1:
-    context = str(input('type the namespace: '))
+    context = str(input('type the context: ')).lower()
+    tag = tag.lower()
     composed_msg = commit_msg_pre + "'" + tag + "(" + context + ")" +": " + msg + "'"
     os.system(composed_msg)
 else:
-    composed_msg = commit_msg_pre + "'" + tag + ": " + msg + "'"
+    composed_msg = commit_msg_pre + "'" + tag.upper() + ": " + msg + "'"
     os.system(composed_msg)
