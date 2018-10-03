ba55fe1fccae38cc4047a8d704df2707e20d9e51
Andre Filho <andre.filho001@outlook.com>
2018-10-02 22:51:56 -0300

    add(tests): placeholders for tests

---
+++ test/test_conventions.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def test_angular_convention():
+    pass
+
+
+def test_changelog_convention():
+    pass
+
+
+def test_symphony_convention():
+    pass
+
+
+def test_no_convention():
+    pass
---
+++ test/test_utils.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+def test_get_text():
+    pass
+
+
+def test_create_file():
+    pass
