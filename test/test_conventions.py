e7d5fc49608f5a8470bf3aa2f50f032c0724c8f3
Arthur Assis <arthur120496@gmail.com>
2018-10-16 22:56:24 -0300

    test: tests symphony convention

--- test/test_conventions.py
+++ test/test_conventions.py
@@ -1,33 +1,36 @@
 import pytest
 import conventions.karma_angular as angular
 import conventions.changelog as changelog
-# import convention.symphony_cmf as symphony
+import conventions.symphony_cmf as symphony
 # import conventions.no_convention as no_convention
 
 
 def test_angular_convention_with_context():
-    message = angular.angular_convention('TAG','message','context')
+    message = angular.angular_convention('TAG', 'message', 'context')
  
     if not ('tag(context): message\n'):
       raise AssertionError()
 
 
 def test_angular_convention_without_context():
-    message = angular.angular_convention('tag','message','')
+    message = angular.angular_convention('tag', 'message', '')
 
     if not ('tag: message\n'):
       raise AssertionError()
 
 
 def test_changelog_convention():
-    message = changelog.changelog_convention('tag','message')
+    message = changelog.changelog_convention('tag', 'message')
 
     if not ('TAG: message\n'):
       raise AssertionError()
 
 
 def test_symphony_convention():
-    pass
+    message = symphony.symphony_convention('tag', 'message')
+
+    if not ('[Tag] message\n'):
+      raise AssertionError()
 
 
 def test_no_convention():
