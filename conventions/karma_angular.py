3f8ece5e8405faa3301b43754f50cd80cd968e68
Andre Filho <andre.filho001@outlook.com>
2018-10-16 22:07:02 -0300

    fix: angular convention logic

--- conventions/karma_angular.py
+++ conventions/karma_angular.py
@@ -2,5 +2,6 @@ def angular_convention(tag, msg, context):
     tag = tag.lower()
     if context is '':
         composed_message = "%s: %s\n" % (tag, msg)
-    composed_message = "%s(%s): %s\n" % (tag, context, msg)
+    else:
+        composed_message = "%s(%s): %s\n" % (tag, context, msg)
     return composed_message
