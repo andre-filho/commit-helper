d9310eca9afe059271d48c27c88a577a15a07801
Arthur Assis <arthur120496@gmail.com>
2018-10-16 23:07:06 -0300

    test: tests just message convention

--- test/test_conventions.py
+++ test/test_conventions.py
@@ -2,36 +2,46 @@ import pytest
 import conventions.karma_angular as angular
 import conventions.changelog as changelog
 import conventions.symphony_cmf as symphony
-# import conventions.no_convention as no_convention
+import conventions.no_convention as no_convention
 
 
 def test_angular_convention_with_context():
     message = angular.angular_convention('TAG', 'message', 'context')
  
     if not ('tag(context): message\n'):
-      raise AssertionError()
+        raise AssertionError()
 
 
 def test_angular_convention_without_context():
     message = angular.angular_convention('tag', 'message', '')
 
     if not ('tag: message\n'):
-      raise AssertionError()
+        raise AssertionError()
 
 
 def test_changelog_convention():
     message = changelog.changelog_convention('tag', 'message')
 
     if not ('TAG: message\n'):
-      raise AssertionError()
+        raise AssertionError()
 
 
 def test_symphony_convention():
     message = symphony.symphony_convention('tag', 'message')
 
     if not ('[Tag] message\n'):
-      raise AssertionError()
+        raise AssertionError()
 
 
 def test_no_convention():
-    pass
+    inputs = [
+        "message", 
+    ]
+
+    def mock_input(s):
+        return inputs.pop(0)
+    no_convention.input = mock_input
+
+    message = no_convention.just_message()
+    if not message == 'Message\n':
+        raise AssertionError()
