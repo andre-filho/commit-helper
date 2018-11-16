42af36a78b4a03a497753c376d98471224e9ffb7
Andre Filho <andre.filho001@outlook.com>
2018-11-16 01:11:45 -0200

    fix(ci): install sh file logic

--- release.sh
+++ release.sh
@@ -2,10 +2,10 @@
 
 python3 setup.py sdist bdist_wheel
 
-if "$1" == "test"; then
+if [ "$1" == "test" ]; then
   twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 fi
 
-if "$1" == "deploy"; then
+if [ "$1" == "deploy" ]; then
   twine upload dist/*
 fi
