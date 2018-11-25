dbbf34bb5bc1f8380cedd8ad06f1e11a48e6bb97
Andre Filho <andre.filho001@outlook.com>
2018-11-25 13:33:24 -0200

    add(conventions helper): symphony convention help

--- commit_helper/conventions/symphony_cmf.py
+++ commit_helper/conventions/symphony_cmf.py
@@ -6,4 +6,22 @@ def symphony_convention(tag, msg):
 
 symphony_convention_help = \
     """
+    General format:
+
+    [<Tag>] <message>
+    <BLANK>
+    <body>
+    <BLANK>
+    <changes>
+    <BLANK>
+    <footer>
+
+    ---
+    Obs.:
+    Tag is not a tag similar to other commit conventions, it is actually a
+    context to what was changed.
+
+    ---
+    Optionals:
+    <body>, <changes>, <footer>
     """
