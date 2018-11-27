cb92895cba411f28949845b9adb1794312603890
Andre Filho <andre.filho001@outlook.com>
2018-11-27 16:56:04 -0200

    fix(hotfix): bug regarding atom convention

--- release.sh
+++ release.sh
@@ -3,9 +3,9 @@
 python3 setup.py sdist bdist_wheel
 
 if [ "$1" == "test" ]; then
-  twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+  twine upload --repository-url https://test.pypi.org/legacy/ dist/* --verbose
 fi
 
 if [ "$1" == "deploy" ]; then
-  twine upload dist/*
+  twine upload dist/* --verbose
 fi
